d284bbe79ad7f5b22e9a616e186dc8a217fbf488
Erica Winne <erica.winne@powerhrg.com>
2021-08-19 21:54:52 -0400

    done

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 10, "M", "brown", "nice", 1);
+INSERT INTO bears VALUES (2,"Rowdy", 5, "M", "black", "nice", 1);
+INSERT INTO bears VALUES (3,"Tabitha", 5, "F", "brown", "nice", 0);
+INSERT INTO bears VALUES (4,"Sargeant Brown", 5, "M", "brown", "nice", 1);
+INSERT INTO bears VALUES (5,"Melissa", 5, "F", "brown", "nice", 1);
+INSERT INTO bears VALUES (6,"Grinch", 10, "M", "Black", "nice", 1);
+INSERT INTO bears VALUES (7,"Wendy", 10, "F", "Brown", "nice", 0);
+INSERT INTO bears VALUES (8,null, 10, "M", "black", "nice", 0);
